e1466ede6f44aaa1fc8f67ad5759dbe05d62ee1e
Klaus Jensen <its@irrelevant.dk>
2019-08-02 15:11:59 +0200

    hw/block/nvme: add basic read/write for zoned namespaces
    This adds basic read and write for zoned namespaces.
    
    A zoned namespace is created by setting the iocs parameter to 0x2 and
    supplying a zero-sized blockdev for zone info persistent state
    (zns.zoneinfo parameter) and the zns.zcap parameter to specify the
    individual zone capacities. The namespace device will compute the
    resulting zone size to be the next power of two and fit in as many zones
    as possible on the underlying namespace blockdev.
    
    If the zone info blockdev pointed to by zns.zoneinfo is non-zero in size
    it will be assumed to contain existing zone state.
    
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>

--- hw/block/nvme-ns.h
+++ hw/block/nvme-ns.h
@@ -23,7 +23,19 @@ typedef struct NvmeNamespaceParams {
     uint32_t nsid;
     uint8_t  iocs;
     uint8_t  lbads;
+
+    struct {
+        uint64_t zcap;
+        uint16_t zoc;
+        uint16_t ozcs;
+    } zns;
 } NvmeNamespaceParams;
+
+typedef struct NvmeZone {
+    NvmeZoneDescriptor zd;
+
+    uint64_t wp_staging;
+} NvmeZone;
 
 typedef struct NvmeNamespace {
     DeviceState  parent_obj;
@@ -41,7 +53,21 @@ typedef struct NvmeNamespace {
     struct {
         uint32_t err_rec;
     } features;
+
+    struct {
+        struct {
+            BlockBackend *blk;
+
+            uint64_t  num_zones;
+            NvmeZone *zones;
+        } info;
+    } zns;
 } NvmeNamespace;
+
+static inline bool nvme_ns_zoned(NvmeNamespace *ns)
+{
+    return ns->iocs == NVME_IOCS_ZONED;
+}
 
 static inline uint32_t nvme_nsid(NvmeNamespace *ns)
 {
@@ -57,15 +83,37 @@ static inline NvmeIdNsNvm *nvme_ns_id_nvm(NvmeNamespace *ns)
     return ns->id_ns[NVME_IOCS_NVM];
 }
 
+static inline NvmeIdNsZns *nvme_ns_id_zoned(NvmeNamespace *ns)
+{
+    return ns->id_ns[NVME_IOCS_ZONED];
+}
+
 static inline NvmeLBAF *nvme_ns_lbaf(NvmeNamespace *ns)
 {
     NvmeIdNsNvm *id_ns = nvme_ns_id_nvm(ns);
     return &id_ns->lbaf[NVME_ID_NS_FLBAS_INDEX(id_ns->flbas)];
 }
 
+static inline NvmeLBAFE *nvme_ns_lbafe(NvmeNamespace *ns)
+{
+    NvmeIdNsNvm *id_ns = nvme_ns_id_nvm(ns);
+    NvmeIdNsZns *id_ns_zns = nvme_ns_id_zoned(ns);
+    return &id_ns_zns->lbafe[NVME_ID_NS_FLBAS_INDEX(id_ns->flbas)];
+}
+
 static inline uint8_t nvme_ns_lbads(NvmeNamespace *ns)
 {
     return nvme_ns_lbaf(ns)->ds;
+}
+
+static inline uint64_t nvme_ns_zsze(NvmeNamespace *ns)
+{
+    return nvme_ns_lbafe(ns)->zsze;
+}
+
+static inline uint64_t nvme_ns_zsze_bytes(NvmeNamespace *ns)
+{
+    return nvme_ns_zsze(ns) << nvme_ns_lbads(ns);
 }
 
 /* calculate the number of LBAs that the namespace can accomodate */
@@ -79,7 +127,62 @@ static inline size_t nvme_ns_blk_state_len(NvmeNamespace *ns)
     return ROUND_UP(DIV_ROUND_UP(nvme_ns_nlbas(ns), 8), BDRV_SECTOR_SIZE);
 }
 
+static inline uint64_t nvme_ns_zone_idx(NvmeNamespace *ns, uint64_t lba)
+{
+    return lba / nvme_ns_zsze(ns);
+}
+
+static inline NvmeZone *nvme_ns_get_zone(NvmeNamespace *ns, uint64_t lba)
+{
+    uint64_t idx = nvme_ns_zone_idx(ns, lba);
+    if (unlikely(idx >= ns->zns.info.num_zones)) {
+        return NULL;
+    }
+
+    return &ns->zns.info.zones[idx];
+}
+
+static inline NvmeZoneState nvme_zs(NvmeZone *zone)
+{
+    return (zone->zd.zs >> 4) & 0xf;
+}
+
+static inline void nvme_zs_set(NvmeZone *zone, NvmeZoneState zs)
+{
+    zone->zd.zs = zs << 4;
+}
+
+static inline bool nvme_ns_zone_wp_valid(NvmeZone *zone)
+{
+    switch (nvme_zs(zone)) {
+    case NVME_ZS_ZSF:
+    case NVME_ZS_ZSRO:
+    case NVME_ZS_ZSO:
+        return false;
+    default:
+        return false;
+    }
+}
+
+static inline uint64_t nvme_zslba(NvmeZone *zone)
+{
+    return le64_to_cpu(zone->zd.zslba);
+}
+
+static inline uint64_t nvme_zcap(NvmeZone *zone)
+{
+    return le64_to_cpu(zone->zd.zcap);
+}
+
+static inline uint64_t nvme_wp(NvmeZone *zone)
+{
+    return le64_to_cpu(zone->zd.wp);
+}
+
 typedef struct NvmeCtrl NvmeCtrl;
+
+const char *nvme_zs_str(NvmeZone *zone);
+const char *nvme_zs_to_str(NvmeZoneState zs);
 
 int nvme_ns_setup(NvmeCtrl *n, NvmeNamespace *ns, Error **errp);
 
